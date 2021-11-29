c5393119ade8ab0ca9b8d080acee32fbfb7f15f7
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-11-30 00:31:47 +0800

    feat(router/tailwind)

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-import { Button } from 'antd'
-
-import {
-  HomeOutlined,
-  SettingFilled,
-  SmileOutlined,
-  SyncOutlined,
-  LoadingOutlined,
-} from '@ant-design/icons';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './views/Home'
+import About from './views/About'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button type="primary">test</Button>
-        <HomeOutlined color="#333" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
